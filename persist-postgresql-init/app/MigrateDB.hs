d39cf681d4a8e9e2a5169158b9a5a0e72f21f5b8
mingyuchoo <mingyuchoo@gmail.com>
2022-08-27 20:14:53 +0900

    added

---
+++ persist-postgresql-init/app/MigrateDB.hs
@@ -0,0 +1,14 @@
+module Main
+    where
+
+import           System.Environment (getArgs)
+
+import           Database           (localConnString, migrateDB)
+import qualified DatabaseEsq        as E
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if null args || head args /= "esq"
+    then migrateDB localConnString
+    else E.migrateDB localConnString
