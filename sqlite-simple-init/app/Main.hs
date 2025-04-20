89e3970d7deb56b5a1e9a3367afbd8c780bf7aa9
mingyuchoo <mingyuchoo@gmail.com>
2025-04-20 16:23:37 +0900

    Reformat imports and improve code alignment in Main.hs and Lib.hs

--- sqlite-simple-init/app/Main.hs
+++ sqlite-simple-init/app/Main.hs
@@ -2,12 +2,8 @@ module Main
     ( main
     ) where
 
-import Lib
-import System.IO
-    ( BufferMode (NoBuffering)
-    , hSetBuffering
-    , stdout
-    )
+import           Lib
+import           System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 
 main :: IO ()
 main = do
--- sqlite-simple-init/src/Lib.hs
+++ sqlite-simple-init/src/Lib.hs
@@ -1,28 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( startApp
-    , User(..)
+    ( User (..)
+    , startApp
     ) where
 
 -- Domain Layer
-import           Domain.Model                (User(..))
+import           Domain.Model                                   (User (..))
 
 -- Application Layer
-import           Application.UserService     (UserService(..))
+import           Application.UserService
+    ( UserService (..)
+    )
 
 -- Infrastructure Layer
-import           Infrastructure.Repository.SQLiteUserRepository (SQLiteUserRepository(..), initDB)
-import           Infrastructure.Web.Server   (startServer)
+import           Infrastructure.Repository.SQLiteUserRepository
+    ( SQLiteUserRepository (..)
+    , initDB
+    )
+import           Infrastructure.Web.Server                      (startServer)
 
 -- Main application entry point that composes all layers
 startApp :: IO ()
 startApp = do
     -- Initialize the database connection
     conn <- initDB
-    
+
     -- Create the repository with the connection
     let repository = SQLiteUserRepository conn
-    
+
     -- Start the web server with the repository
     startServer repository
